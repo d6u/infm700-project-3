edf3ed1605a63d0ab10256928a6749ed6e09dae2
scd5029 <sdodge@umd.edu>
2013-11-10 14:05:19 -0500

    404 Page
    Added a 404 page for the site

---
+++ patterson-park/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+		
+	<h1>Oops!  This page cannot be found.</h1>
+	<br/>
+	<p>We are sorry.  The page that you are looking for cannot be found.  You can try again from the <a href = "http://infm700-project-3.daiwei.lu/" title = "Home Page">home page</a> or if you think something is wrong, please <a href = "mailto:friends@pattersonpark.com" title = "Email the Friends of Patterson Park">contact us</a>.</p>
+
+
+
+<?php get_footer(); ?>
