4b30be45c6c7c55e4aafdf1cd92197706c256095
scd5029 <sdodge@umd.edu>
2013-11-11 13:03:03 -0500

    Facebook Integration v3
    The documentation was wrong and turns out I needed to add 'http://'.

--- patterson-park/content-page-body.php
+++ patterson-park/content-page-body.php
@@ -3,7 +3,7 @@
   <div class="col-md-9 col-sm-8">
     <div class="se-post-body-content">
       <?php the_content(); ?>
-	  <div class="fb-like" data-href="<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+	  <div class="fb-like" data-href="<?php echo 'http://'.$_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
       <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
       <footer class="entry-meta">
         <?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
