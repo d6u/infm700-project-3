fba2e850047b79c77d01fde172902386b755c360
Daiwei Lu <daiweilu123@gmail.com>
2013-11-18 00:08:53 -0500

    Add favicon to theme director

--- patterson-park/header.php
+++ patterson-park/header.php
@@ -11,6 +11,7 @@
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <meta name="viewport" content="width=device-width">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
   <?php wp_head(); ?>
 </head>
 
